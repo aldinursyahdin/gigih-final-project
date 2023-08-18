6457c761b080f04ba0ba8407e9e1c576b15b79cf
aldinursyahdin <72461590+aldinursyahdin@users.noreply.github.com>
2023-08-19 00:05:16 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json())
-app.use(cors());
+app.use(cors(
+    {origin: 'https://gigih-final-project-murex.vercel.app'}));
 
 
 
